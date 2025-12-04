f0f3bb4b2f36c16679f5abe5d697364c766e4b37
Nocrantus <nocrantus@gmail.com>
2025-12-04 15:34:08 -0500

    working version

--- main.js
+++ main.js
@@ -3,22 +3,58 @@ let dates = [];
 let sliderVal = 1655352000; //initialize sliderVal
 let output = document.getElementById("output");
 let button = document.getElementById("myButton");
+output.innerHTML = "6/16/2022";
 
-// Add an event listener to the button
-
-
+let val_name = "death"; //case, death, or rate
 
 d3.json("ct-towns-2022-simple-datactgov.geojson").then((geojson,err1)=> {
     d3.dsv(",","covid_imp.csv",(d)=>{
-        return{ date:  d.date, name: d.city, case: +d.case, death: +d.death, rate: +d.rate};
+        return{
+            date:  d.date,
+            name: d.city,
+            case: +d.case, case_imp: +d.case_imp,
+            death: +d.death, death_imp: +d.death_imp,
+            rate: +d.rate};
     }).then((data,err2)=>{
         console.log(data);
+
+        //Slider Code
         let slider = document.getElementById("myRange");
         // Update the current slider value (each time you drag the slider handle)
         slider.oninput = function() {
-            sliderVal = this.value;
+            sliderVal = findClosest(dates,this.value);
             console.log(sliderVal);
-            console.log(findClosest(dates, sliderVal));
+
+            let date = new Date(sliderVal * 1000);
+            let year = date.getFullYear();
+            let month = date.getMonth()+1;
+            let day = date.getDate();
+            let formattedTime = month + '/' + day + '/' + year;
+
+            output.innerHTML = formattedTime;
+        }
+
+        //Separates the values and dates from the main array
+        for (let j =0; j < data.length; j++) {
+            vals.push(data[j][val_name]);
+            dates.push(data[j]["date"]);
+        }
+
+        let max = d3.max(vals);
+        let min = d3.min(vals);
+        let mid = min + (max-min)/2;
+
+        let continuousColor = d3.scaleLinear([min,max],[0,1.0]); //continuous version
+        let colorScale = d3.interpolateHsl(d3.hsl(120,1,0.5),d3.hsl(0,1.0,0.5)); //1 = green, 0 = red
+
+        function discreteColor(value)
+        {
+            if (value >= mid){
+                return 1;
+            }
+            else{
+                return 0;
+            }
         }
 
         //Finds the closest value in 'array' to 'target'
@@ -34,63 +70,6 @@ d3.json("ct-towns-2022-simple-datactgov.geojson").then((geojson,err1)=> {
             return res;
         }
 
-        //separating data values from main array
-        for (let j = 0; j < data.length; j++) {
-            ///vals.push(data[j]["case"]);
-            if (data[j+1]["date"] !== data[j]["date"])
-            {
-                dates.push(data[j]["date"]);
-            }
-        }
-
-        //basic logic flow: if the date is the one given by slider, assign corresponding case value.
-        function assignData() {
-            console.log("Running");
-            for (let i = 0; i < geojson.features.length; i++) {
-                for (let j = 0; j < data.length; j++) {
-                    if (data[j]["date"] == ) {
-                        console.log("date found");
-                        if (geojson.features[i].properties["name"] === data[j]["name"]) {
-                            geojson.features[i].properties["val"] = data[j]["case"];
-                        }
-                    }
-                    if (j == 0){
-                        vals.push(data[j]["case"]);
-                    }
-                }
-            }
-            console.log("Done");
-        }
-        console.log(vals);
-        console.log(dates);
-        let max = d3.max(vals);
-        let min = d3.min(vals);
-        let mid = min + (max-min)/2;
-        console.log(max);
-        console.log(min);
-        console.log(mid);
-
-        let continuousColor = d3.scaleLinear([min,max],[0,1.0]); //continuous version
-        let colorScale = d3.interpolateHsl(d3.hsl(120,1,0.5),d3.hsl(0,1.0,0.5)); //1 = green, 0 = red
-
-        function discreteColor(value)
-        {
-            if (value >= mid){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-
-
-        let map = L.map('map').setView([41.38016733657364, -72.10705729845692], 8); //new leaflet map
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-
-
         function townStyle(feature){
             return {
                 "color": colorScale(continuousColor(feature.properties["val"])), //use colormap or colorscale
@@ -100,13 +79,48 @@ d3.json("ct-towns-2022-simple-datactgov.geojson").then((geojson,err1)=> {
 
         function onEachFeature(feature, layer){
             if (feature.properties && feature.properties.val) {
-                layer.bindPopup("Case number in " + feature.properties.name + ": " + feature.properties.val.toLocaleString('en-US')); //It doesn't work without the empty string?
+                layer.bindPopup(val_name + " number in " + feature.properties.name + ": " + feature.properties.val.toLocaleString('en-US')); //It doesn't work without the empty string?
             }
         }
 
-        let geoJsonLayer = L.geoJSON(geojson,{style:townStyle, onEachFeature:onEachFeature}).addTo(map);
+        let map = L.map('map').setView([41.38016733657364, -72.10705729845692], 8); //new leaflet map
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
 
-        document.getElementById("resetButton").addEventListener("click", assignData);
-        document.getElementById("resetButton").addEventListener("click", function () {geoJsonLayer.resetStyle(this);});
+        let layerGroup = new L.LayerGroup();
+        layerGroup.addTo(map);
+
+        generateMap(findClosest(dates, 1615252500.0)); //Initial generation
+        //basic logic flow: if the date is the one given by slider, assign corresponding case value.
+        function generateMap(chosenDate, clicked){
+            console.log("Running");
+            for (let i = 0; i < geojson.features.length; i++) {
+                for (let j = 0; j < data.length; j++) {
+                    if (data[j]["date"] === chosenDate) {
+                        if (geojson.features[i].properties["name"] === data[j]["name"]) {
+                            geojson.features[i].properties["val"] = data[j][val_name];
+                        }
+                    }
+                }
+            }
+
+            let geojsonLayer = L.geoJSON(geojson,{style:townStyle, onEachFeature:onEachFeature});
+            if (clicked) { // Check if the layer exists on the map
+                layerGroup.clearLayers();
+                console.log("removed");
+            }
+            layerGroup.addLayer(geojsonLayer);
+            layerGroup.addTo(map);
+            console.log("added")
+        }
+
+        // Add an event listener to the button
+        document.getElementById("resetButton").addEventListener("click", function (){
+            console.log("clicked!");
+            generateMap(findClosest(dates, sliderVal), true);
+        });
     });
+
 });
